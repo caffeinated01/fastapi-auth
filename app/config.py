3883f85e767c690ca194a92d84f1174782fc2c0a
Javier <106571756+caffeinated01@users.noreply.github.com>
2025-11-27 15:55:48 +0800

    shift .env to root folder

--- app/config.py
+++ app/config.py
@@ -2,10 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="../.env")
 
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-settings = Settings()
+
+settings = Settings()  # type: ignore
