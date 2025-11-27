3092cb5957cca37d778f53494b617b32fcdd0722
Javier <106571756+caffeinated01@users.noreply.github.com>
2025-11-27 13:50:50 +0800

    add extra dependency to check if user is active

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
@@ -5,6 +6,7 @@ from pydantic import ValidationError
 
 from app.config import settings
 from app.db.database import SessionDep
+from app.models.user import User
 from app.schemas.auth import TokenPayload
 from app.crud import user as user_crud
 
@@ -12,7 +14,7 @@ from app.crud import user as user_crud
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
-def get_access_token(token: str = Depends(oauth2_scheme)) -> TokenPayload:
+def get_access_token(token: Annotated[str, Depends(oauth2_scheme)]) -> TokenPayload:
     try:
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
@@ -31,7 +33,7 @@ def get_access_token(token: str = Depends(oauth2_scheme)) -> TokenPayload:
     return token_payload
 
 
-def get_current_user(db: SessionDep, token: TokenPayload = Depends(get_access_token)):
+def get_current_user(db: SessionDep, token: Annotated[TokenPayload, Depends(get_access_token)]):
     user = user_crud.get_user_by_username(db, username=token.sub)
 
     if not user:
@@ -41,3 +43,13 @@ def get_current_user(db: SessionDep, token: TokenPayload = Depends(get_access_to
         )
 
     return user
+
+
+def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user",
+        )
+
+    return current_user
